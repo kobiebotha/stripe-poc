d3e7fc8ff125df22e9b3ace69965e7e5cd25206e
Kobie <kobie@journeyapps.com>
2021-09-15 18:27:38 +0000

    stash

--- mobile/main.ts
+++ mobile/main.ts
@@ -1,6 +1,11 @@
+let stripeLoaded = false;
+let stripeUrl = '';
+
 // This function is called when the app navigates to this view (using a link)
 function init() {
     // initialize any data here that should be available when the view is shown
+    console.log('it ran');
+    view.myvar = '4';
 }
 
 // This function is called when the user returns to this view from another view
@@ -11,9 +16,17 @@ function resume(from) {
     // if any data needs to be refreshed when the user returns to this view, you can do that here:
 }
 
-
 function buttonPress() {
   // TODO implement
 
   component.html({id: 'stripe'})
+}
+
+function getSrc() {
+    return !stripeLoaded? '@local/stripe/index.html' : stripeUrl;
+}
+
+function loadStripe() {
+    stripeUrl = 'https://checkout.stripe.com/pay/cs_test_a11TtlQDF9PtjHwVvhFYLzMvD6u8Ht6ptW7LcprXZFatpsAe423VyHTH3Q#fidkdWxOYHwnPyd1blpxYHZxWlVkNmpcMHZtQWp9NGFJf0lCb2hXcEpPVjU1NktfM2JDbFcnKSdjd2poVmB3c2B3Jz9xd3BgKSdpZHxqcHFRfHVgJz8ndmxrYmlgWmxxYGgnKSdga2RnaWBVaWRmYG1qaWFgd3YnP3F3cGB4JSUl';
+    stripeLoaded = true;
 }
